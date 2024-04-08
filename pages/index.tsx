409c565a958924a6f2f5d1180e74d1c55d255f3f
pumpkinpies <ziadmhmh@gmail.com>
2024-04-09 01:24:46 +0300

    Aded mui and mofieid index.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
-import { Progress } from "@/components/ui/progress";
+import LinearProgressWithLabel from "@/components/LinearProgressWithLabel";
+import LinearProgress, {
+    LinearProgressProps,
+} from "@mui/material/LinearProgress";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return <main className="p-10 text-stone-500 "> Ziad </main>;
+    const [state, setState] = useState<number>(0);
+    useEffect(() => {
+        const interval = setInterval(
+            () => setState((prev) => (prev !== 100 ? prev + 10 : 0)),
+            2000
+        );
+        () => clearInterval(interval);
+    }, []);
+    return (
+        <main className="p-10 text-stone-500 grid gap-10 items-center ">
+            <h1>Ziad is cool</h1>
+            <h2>Anas is gays 🌈</h2>
+            <LinearProgressWithLabel value={state} variant="determinate" />
+        </main>
+    );
 }
-
-<Progress value={33} />;
